f4d5573c77e85be148f536c1fde2100929620b85
laval.geek@outlook.fr <Laval131202>
2022-02-21 16:30:42 +0100

    Commit et push fin de seance TD4

--- app/router.js
+++ app/router.js
@@ -6,4 +6,7 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  // eslint-disable-next-line ember/no-capital-letters-in-routes
+  this.route('testNew');
+});
